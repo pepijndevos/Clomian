b6cdd30cf09193219f6f2f923fe855dcf219bceb
Pepijn de Vos <pepijndevos@gmail.com>
2010-11-02 20:28:49 +0100

    more blocks and a cli

--- src/clomian.clj
+++ src/clomian.clj
@@ -3,21 +3,100 @@
   (:use [incanter core charts])
   (:gen-class))
 
-(set! *warn-on-reflection* true)
+;(set! *warn-on-reflection* true)
 
-(def types {0  "air"
-            7  "Bedrock"
-            39 "Brown Mushroom"
-            40 "Red Mushroom"
-            10 "Lava"
-            11 "Stationary Lava"
-            13 "Gravel"
-            49 "Obsidian"
+(def types {0 "Air"
+            4 "Cobblestone"
+            8 "Water"
+            12 "Sand"
+            16 "Coal ore"
+            20 "Glass"
+            24 "Lime cloth"
+            28 "Blue cloth"
+            32 "Magenta cloth"
+            36 "White cloth"
+            40 "Red mushroom"
+            44 "Step"
+            48 "Mossy cobblestone"
+            52 "Mob spawner"
+            56 "Diamond ore"
+            60 "Soil"
+            64 "Wooden door"
+            68 "Wall sign"
+            72 "Wooden pressure plate"
+            76 "Redstone torch (on)"
+            80 "Snow block"
+            84 "Jukebox"
+            88 "Slow sand"
+            3 "Dirt"
+            7 "Bedrock"
+            11 "Stationary lava"
+            15 "Iron ore"
+            19 "Sponge"
+            23 "Yellow cloth"
+            27 "Cyan cloth"
+            31 "Violet cloth"
+            35 "Gray / white cloth"
+            39 "Brown mushroom"
+            43 "Double step"
+            47 "Bookshelf"
             51 "Fire"
+            55 "Redstone wire"
+            59 "Crops"
+            63 "Sign post"
+            67 "Cobblestone stairs"
+            71 "Iron door"
+            75 "Redstone torch (off)"
+            79 "Ice"
+            83 "Reed"
             87 "Bloodstone"
-            88 "Slow Sand"
-            89 "Lightstone"
-            90 "Portal"})
+            91 "Jack-o-lantern"
+            2 "Grass"
+            6 "Sapling"
+            10 "Lava"
+            14 "Gold ore"
+            18 "Leaves"
+            22 "Orange cloth"
+            26 "Aqua green cloth"
+            30 "Indigo cloth"
+            34 "Black cloth"
+            38 "Red rose"
+            42 "Iron block"
+            46 "TNT"
+            50 "Torch"
+            54 "Chest"
+            58 "Workbench"
+            62 "Burning furnace"
+            66 "Minecart tracks"
+            70 "Stone pressure plate"
+            74 "Glowing redstone ore"
+            78 "Snow"
+            82 "Clay"
+            86 "Pumpkin"
+            90 "Portal"
+            1 "Stone"
+            5 "Wood"
+            9 "Stationary water"
+            13 "Gravel"
+            17 "Log"
+            21 "Red cloth"
+            25 "Green cloth"
+            29 "Purple cloth"
+            33 "Pink cloth"
+            37 "Yellow flower"
+            41 "Gold block"
+            45 "Brick"
+            49 "Obsidian"
+            53 "Wooden stairs"
+            57 "Diamond block"
+            61 "Furnace"
+            65 "Ladder"
+            69 "Lever"
+            73 "Redstone ore"
+            77 "Stone button"
+            81 "Cactus"
+            85 "Fence"
+            89 "Lightstone"})
 
 (defn blocks [file]
   (-> ^java.io.File file
@@ -40,8 +119,9 @@
 (defn plotfn [freqs btype layer]
   (get (nth freqs layer) (byte btype) 0))
 
-(defn -main [& options]
-  (let [fr (file-seq (clojure.java.io/file "/Users/pepijndevos/Library/Application Support/minecraft/saves/World2/DIM-1/"))
+(defn -main [path & options]
+  (let [options (set (map #(Integer. %) options))
+        fr (file-seq (clojure.java.io/file path))
         fr (mapcat blocks (filter #(and (.isFile ^java.io.File %) (not (.isHidden ^java.io.File %))) fr))
         fr (freqs fr)
         canvas (-> (reduce #(add-function %1 (partial plotfn fr (key %2)) 0 128
@@ -50,7 +130,7 @@
                                     :x-label "Layer"
                                     :y-label "Blocks"
                                     :legend true)
-                            types))]
+                            (select-keys types options)))]
     (slider #(set-y-range canvas 0 %) (range 0 500))
     (view canvas)))
     ;(save canvas "graph.png")
