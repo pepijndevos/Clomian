5c9e6c3d3543ba334b60e9180f24e24ba33b643a
Pepijn de Vos <pepijndevos@gmail.com>
2010-11-02 17:03:48 +0100

    fixed the stackoverflow

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Minecraft Ananlysis"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [incanter "1.2.3-SNAPSHOT"]])
+                 [incanter "1.2.3-SNAPSHOT"]]
+  :main clomian)
--- src/clomian.clj
+++ src/clomian.clj
@@ -1,6 +1,9 @@
 (ns clomian
   (:require clojure.java.io)
-  (:use [incanter core charts]))
+  (:use [incanter core charts])
+  (:gen-class))
+
+(set! *warn-on-reflection* true)
 
 (def types {0  "air"
             7  "Bedrock"
@@ -17,37 +20,38 @@
             90 "Portal"})
 
 (defn blocks [file]
-  (-> file
+  (-> ^java.io.File file
     java.io.FileInputStream.
     org.jnbt.NBTInputStream.
     .readTag
     .getValue
-    (get "Level")
+    (.get "Level")
     .getValue
-    (get "Blocks")
+    (.get "Blocks")
     .getValue))
 
 (defn freqs [blocks]
   (->> blocks
     (partition 128)
     (reduce (fn [counts col]
-              (pmap #(update-in %1 [%2] (fnil inc 0)) counts col))
+              (doall (map #(update-in %1 [%2] (fnil inc 0)) counts col)))
             (take 128 (repeat {})))))
 
 (defn plotfn [freqs btype layer]
   (get (nth freqs layer) (byte btype) 0))
 
-(let [fr (file-seq (clojure.java.io/file "/Users/pepijndevos/Library/Application Support/minecraft/saves/World2/DIM-1/"))
-      fr (mapcat blocks (filter #(and (.isFile %) (not (.isHidden %))) fr))
-      fr (freqs fr)
-      canvas (-> (reduce #(add-function %1 (partial plotfn fr (key %2)) 0 128
-                                        :series-label (val %2))
-                         (xy-plot [] []
-                                  :x-label "Layer"
-                                  :y-label "Blocks"
-                                  :legend true)
-                          types))]
-  (slider #(set-y-range canvas 0 %) (range 0 500))
-  (view canvas))
-  ;(save canvas "graph.png")
-  ;(save (set-y-range canvas 0 50) "graph-low.png"))
+(defn -main [& options]
+  (let [fr (file-seq (clojure.java.io/file "/Users/pepijndevos/Library/Application Support/minecraft/saves/World2/DIM-1/"))
+        fr (mapcat blocks (filter #(and (.isFile ^java.io.File %) (not (.isHidden ^java.io.File %))) fr))
+        fr (freqs fr)
+        canvas (-> (reduce #(add-function %1 (partial plotfn fr (key %2)) 0 128
+                                          :series-label (val %2))
+                           (xy-plot [] []
+                                    :x-label "Layer"
+                                    :y-label "Blocks"
+                                    :legend true)
+                            types))]
+    (slider #(set-y-range canvas 0 %) (range 0 500))
+    (view canvas)))
+    ;(save canvas "graph.png")
+    ;(save (set-y-range canvas 0 50) "graph-low.png")))
